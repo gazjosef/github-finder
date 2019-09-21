14768569b03ca440cd8cf382a344e96ec68eaf7b
gazjosef <39395712+gazjosef@users.noreply.github.com>
2019-09-21 19:30:22 +1000

    added prop types

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import NavBar from "./Components/Layout/Navbar";
+
 class App extends Component {
   render() {
-    const name = "John Doe";
-    const loading = false;
-    const showName = true;
-
     return (
       <div className="App">
-        {loading ? (
-          <h4>Loading...</h4>
-        ) : (
-          <h1>Hello {showName && name}</h1>
-          // <h1>Hello {showName ? name : null}</h1>
-        )}
+        <NavBar title="Github Finder" icon="fab fa-github" />
       </div>
     );
   }
